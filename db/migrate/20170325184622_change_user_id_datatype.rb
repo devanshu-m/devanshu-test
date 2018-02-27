89ba9b8c58439ad383ef86c1fb3a7abb275cb3cc
devanshu-m <dmatlawala@culverhouse.ua.edu>
2018-02-27 15:29:29 -0600

    adding provider for travis

--- db/migrate/20170325184622_change_user_id_datatype.rb
+++ db/migrate/20170325184622_change_user_id_datatype.rb
@@ -1,6 +1,6 @@
 class ChangeUserIdDatatype < ActiveRecord::Migration
   def change
-    change_column :posts, :user_id, :integer
-    #change_column :posts, :user_id, 'integer USING CAST(user_id AS integer)'
+    #change_column :posts, :user_id, :integer
+    change_column :posts, :user_id, 'integer USING CAST(user_id AS integer)'
   end
 end
